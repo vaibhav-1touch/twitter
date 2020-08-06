96b1656051e735018759e3637fc136493f2a8853
Vaibhav <vaibhav@1touch-dev.com>
2020-08-06 14:47:40 +0530

    Following followers

--- zombie_script.py
+++ zombie_script.py
@@ -8,8 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
-search_keyword = "TechnicalGuruji"
-
+search_keyword = ""
 
 # Functions in order ->
 # get_cookies()             Load cookies from previous session with cookies.pkl file
@@ -18,7 +17,7 @@ search_keyword = "TechnicalGuruji"
 # login()                   Enter user credentials on login page
 # search_bar()              Searches the user handle and open main page
 # open_followers()          Click on followers button to open the followers scroll menu
-# write_names()             Get names from followers button and save it to account.txt
+# random_follow()           Follow random ids and get names from followers button and save it to account.txt
 
 
 load_dotenv()
@@ -43,17 +42,21 @@ def save_cookies():
 
 
 def get_account():
-    name = []
-    with open('accounts.txt','r') as f:
-        name = f.read().split('\n')
-        print(name[0])
-        user_handle = name[0]
+    global search_keyword
 
-    print(name)
-    # name.remove(name[0])
-    name = [x for x in name if x]
+    if search_keyword == "":
+        name = []
+        with open('accounts.txt','r') as f:
+            name = f.read().split('\n')
+            search_keyword = name[0]
+            user_handle = name[0]
 
-    return name
+        name.remove(name[0])
+        name = [x for x in name if x]
+        with open('accounts.txt','w') as f:
+            for i in name:
+                f.write(i)
+                f.write('\n')
 
 
 def login():
@@ -86,49 +89,68 @@ def search_bar(search=search_keyword):
                 time.sleep(0.4 * random.random())
     
     time.sleep(random.randint(4, 6) + random.random())
-    count = 0
-    dropdown_accounts_div_class = ".css-1dbjc4n"
-    for i in driver.find_elements_by_css_selector(dropdown_accounts_div_class):
-        for j in i.find_elements_by_tag_name('div')[850:]:
-            print(count)
-            count += 1
-            for k in j.find_elements_by_tag_name('span'):
-                if j.get_attribute('role') == "button" and k.text.strip('@') == search_keyword:
-                    ActionChains(driver).move_to_element(j).click().perform()
-                    time.sleep(random.randint(4, 6) + random.random())
-                    return
 
+    list_child_class = ".css-4rbku5.css-1dbjc4n.r-1ny4l3l.r-1j3t67a.r-1w50u8q"
+    list_class = driver.find_elements_by_css_selector(list_child_class)
+    for i in list_class:
+        if i.get_attribute('data-testid') == "TypeaheadUser":
+            parent = i.find_element_by_xpath('..')
+            i.click()
+            break
+
+    time.sleep(random.randint(4, 5) + random.random())
+    
 
 def open_followers():
-    follow_button_parent_div_class = ".css-1dbjc4n.r-18u37iz.r-1w6e6rj"
+    follow_button_child_div_class = ".css-901oao.css-16my406.r-1qd0xha.r-vw2c0b.r-ad9z0x.r-bcqeeo.r-qvutc0"
+    name = 'https://twitter.com/' + search_keyword + '/followers'
+    for i in driver.find_elements_by_css_selector(follow_button_child_div_class):
+        elem = i.find_element_by_xpath('..')
+        print(elem.get_attribute('href'))
+        if elem.get_attribute('href') == name:
+            ActionChains(driver).move_to_element(i).click().perform()
+            break
+
+    time.sleep(random.randint(4, 5) + random.random())
+    
+
+def random_follow():
+    div_class_followers_list = ".css-1dbjc4n.r-1awozwy.r-18u37iz.r-1wtj0ep"
+
+    div_name_class = driver.find_elements_by_css_selector(".css-1dbjc4n.r-1wbh5a2.r-dnmrzs")
+    div_followers_class = driver.find_elements_by_css_selector(".css-1dbjc4n.r-1n0xq6e.r-bcqeeo")
     count = 0
-    name = '/' + search_keyword + '/followers'
-    print(name)
-    for i in driver.find_elements_by_css_selector(follow_button_parent_div_class):
-        for j in i.find_elements_by_tag_name('a'):
-            print(count)
-            count = count + 1
-            print(j.get_attribute('href'))
-            if j.get_attribute('href') == name:
-                print('if loop worked')
-                i.click()
-                time.sleep(random.randint(1, 2) + random.random())
-                return
+    accounts =  []
+    for i in driver.find_elements_by_css_selector(div_class_followers_list):
+        for j, k in zip(div_name_class, div_followers_class):
 
+            for names in j.find_elements_by_tag_name('a'):
+                accounts.append(names.get_attribute("href").strip("https://twitter.com/"))
+                print(f'Following {names.get_attribute("href").strip("/")}')
 
-def write_names():
-    name_div_class = ".css-1dbjc4n.r-1wbh5a2.r-dnmrzs"
-
-    names = driver.find_elements_by_css_selector(name_div_class)
-    for i in names:
-        for j in i.find_elements_by_tag_name('a'):
-            print(j.get_attribute('href').strip('/'))
+            for button in k.find_elements_by_xpath('//div'):
+                if button.get_attribute('role') == "button":
+                    for m in button.find_elements_by_tag_name('span'):
+                        print(m.text)
+                        if m.text == "Follow" and count < 5 and random.random() > 0.7:
+                            ActionChains(driver).move_to_element(m).click().perform()
+                            time.sleep(random.randint(2,4) + random.random())
+                            count += 1
+                            break
+    
+    with open('accounts.txt', 'a') as f:
+        for i in accounts:
+            f.write(i)
+            f.write('\n')
+        
 
 
 if __name__ == "__main__":
-    # get_cookies()
+    get_account()
+    print(search_keyword)
+    get_cookies()
     login()
-    # save_cookies()
-    search_bar()
+    save_cookies()
+    search_bar(search_keyword)
     open_followers()
-    write_names()
+    random_follow()
